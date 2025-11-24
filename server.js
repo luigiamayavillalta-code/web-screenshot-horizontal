3860f636d3ce3241ceba835b836118a7f1557c8a
luigiamayavillalta-code <luigiamayavillalta@gmail.com>
2025-11-24 07:46:47 -0600

    Initialize server with Express and Puppeteer

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/screenshot', async (req, res) => {
+  const { ur
